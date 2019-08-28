cfe7c86af0db255b523fdda104eb5f311cc9a119
Peng Li <peng.li@compass.com>
2019-08-28 22:40:27 +0800

    typescript-react--formik--with-formik-demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-TypeScript React Formik Demo
+TypeScript React Formik "withFormik" Demo
 =================================
 
-合理使用Formik的话，好像可以让form的验证以及设值等，变得简单一点。
+使用`withFormik`可以把一个把formik自己的参数与form的定义分离开。
+
+注意：还有一个`connect`是用于field级别的，本demo的`withFormik`是form级别的。
 
 ```
 npm install
--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,61 +1,62 @@
 import React from 'react'
-import {Formik, FormikProps, FormikActions, Form, Field, ErrorMessage} from 'formik';
+import {FormikProps, FormikActions, Form, Field, ErrorMessage, withFormik} from 'formik';
 
 type LoginProps = {
   username: string,
   password: string,
 }
 
-export default function MyForm() {
-  const initialValues: LoginProps = {
-    username: 'aaa',
-    password: '',
-  };
+function generatePassword(form: FormikProps<LoginProps>) {
+  form.setFieldValue('password', '123456');
+}
 
-  function onSubmit(values: LoginProps, actions: FormikActions<LoginProps>) {
-    console.log({values, actions});
-    alert(JSON.stringify(values, null, 2));
-    actions.setSubmitting(false)
-  }
-
-  function validate(values: LoginProps) {
-    const errors: Partial<LoginProps> = {};
-    if (!values.username) {
-      errors.username = 'Required';
-    }
-    if (!values.password) {
-      errors.password = 'Required';
-    }
-    return errors;
-  }
-
-  function generatePassword(form: FormikProps<LoginProps>) {
-    form.setFieldValue('password', '123456');
-  }
+export function MyForm(props: FormikProps<LoginProps>) {
+  console.log('props', props);
 
   return <div>
     <h1>Hello Formik</h1>
-    <Formik initialValues={initialValues}
-            validate={validate}
-            onSubmit={onSubmit}>
-      {(formikBag: FormikProps<LoginProps>) =>
-        <Form>
-          <div>
-            <Field type="text" name='username'/>
-            <ErrorMessage name="username" component="span"/>
-          </div>
-          <div>
-            <label>Password:
-              <Field type='password' name='password'/>
-            </label>
-            <button type='button' onClick={() => generatePassword(formikBag)}>Generate Password</button>
-            <ErrorMessage name="password" component="span"/>
-          </div>
-          <div>
-            <button>Login</button>
-          </div>
-        </Form>
-      }
-    </Formik>
+
+    <Form>
+      <div>
+        <Field type="text" name='username'/>
+        <ErrorMessage name="username" component="span"/>
+      </div>
+      <div>
+        <label>Password:
+          <Field type='password' name='password'/>
+        </label>
+        <button type='button' onClick={() => generatePassword(props)}>Generate Password</button>
+        <ErrorMessage name="password" component="span"/>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </Form>
   </div>
-};
+}
+
+function validate(values: LoginProps) {
+  const errors: Partial<LoginProps> = {};
+  if (!values.username) {
+    errors.username = 'Required';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  }
+  return errors;
+}
+
+function handleSubmit(values: LoginProps, actions: FormikActions<LoginProps>) {
+  console.log({values, actions});
+  alert(JSON.stringify(values, null, 2));
+  actions.setSubmitting(false)
+}
+
+export default withFormik<{}, LoginProps>({
+  mapPropsToValues: () => ({
+    username: 'aaa',
+    password: ''
+  }),
+  validate,
+  handleSubmit,
+})(MyForm)
